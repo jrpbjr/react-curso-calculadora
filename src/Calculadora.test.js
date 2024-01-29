995a6652a8daeb24abc627cfac98c3c8a4d76125
Jose Roberto Paes Barbosa Junior <jrpbjr@gmail.com>
2024-01-29 20:37:48 -0300

    Interface HTML

--- src/Calculadora.test.js
+++ src/Calculadora.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Calculadora from './Calculadora';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Calculadora />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
