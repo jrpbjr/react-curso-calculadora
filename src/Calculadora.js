c8c8b7dc1d4fbf81d277c09b0e4d7683167a4d1b
Jose Roberto Paes Barbosa Junior <jrpbjr@gmail.com>
2024-01-30 23:06:18 -0300

    Servico da calculadora no componente principal

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -3,19 +3,61 @@ import './Calculadora.css';
 import { 
   Jumbotron, Container, Row, Col, Button, Form 
 } from 'react-bootstrap';
+import CalculadoraService from './Calculadora.Service';
 
 function Calculadora() {
 
+  const [calcular, concatenarNumero, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO] = CalculadoraService();
+
   const [txtNumeros, setTxtNumeros] = useState('0');
+  const[numero1, setNumero1] = useState('0');
+  const[numero2, setNumero2] = useState(null);
+  const[operacao,setOperacao] = useState(null);
+
 
   function adicionarNumero(numero){
-    setTxtNumeros(txtNumeros + numero);
+    let resultado;
+    if(operacao === null){
+      resultado = concatenarNumero(numero1, numero);
+      setNumero1(resultado);
+    }else{
+      resultado = concatenarNumero(numero2, numero);
+      setNumero2(resultado)
+    }
+
+    setTxtNumeros(resultado);
   }
 
   function definirOperacao(op){
-    setTxtNumeros(op);
+    // apenas definir a operação caso ela não exista
+    if(operacao === null){
+      setOperacao(op);
+      return;
+    }
+    // caso operação estiver definida e numero 2 selecionado, realiza o cálculo da operação
+    if (numero2 !== null){
+      const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao)
+      setOperacao(op);
+      setNumero1(resultado.toString());
+      setNumero2(null);
+      setTxtNumeros(resultado.toString());
+    }
   }
 
+  function acaoCalcular(){
+    if (numero2 === null){
+      return;
+    }
+    const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao);
+    setTxtNumeros(resultado);
+  }
+
+  function limpar(){
+    setTxtNumeros('0');
+    setNumero1('0');
+    setNumero2(null);
+    setOperacao(null);
+  }
 
   return (
     <Jumbotron style={{
@@ -28,7 +70,9 @@ function Calculadora() {
         <Container>
           <Row>
             <Col xs="3">
-              <Button variant="danger">C</Button>
+              <Button variant="danger" onClick=
+              {limpar}
+              >C</Button>
             </Col>
             <Col xs="9">
               <Form.Control type="text"
@@ -113,10 +157,14 @@ function Calculadora() {
               >0</Button>
             </Col>
             <Col>
-              <Button variant='light'>.</Button>
+              <Button variant='light'onClick=
+              {()=>{adicionarNumero('.')}}
+              >.</Button>
             </Col>
             <Col>
-              <Button variant='success'>=</Button>
+              <Button variant='success'onClick=
+              {acaoCalcular}
+              >=</Button>
             </Col>
             <Col>
               <Button variant='warning'  onClick=
